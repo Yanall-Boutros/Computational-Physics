e19212c9bf24d4e7dd3fc3325054e445931d42fb
Yanall-Boutros <yboutros@ucsc.edu>
2019-04-08 22:50:06 -0700

    Problem 5 finished

---
+++ problem_sets/ps1/locals/3/prob3c.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+def Sup(n):
+    total = 0
+    vals = np.float128(np.linspace(1, n, n))
+    for num in vals:
+        total += np.float128(num**-1)
+    return total
+def Sdown(n):
+    total = 0
+    vals = np.float128(np.linspace(n, 1, n))
+    for num in vals:
+        total += num**-1
+    return total
+def logcomp(up, down):
+    up = np.float128(up)
+    down = np.float128(down)
+    if up > down: return (up - down)/(np.abs(up) + np.abs(down))
+    return (down - up)/(np.abs(up) + np.abs(down))
+comps = []
+nvals = []
+print("Summing up: \t \t Summing down:")
+for n in range(2, 8):
+    N = 10**n
+    nvals.append(N)
+    sup = Sup(N)
+    sdown = Sdown(N)
+    comps.append(logcomp(sup, sdown))
+    print(sup, "\t", sdown)
+comps = np.array(comps)
+nvals = np.array(nvals)
+plt.loglog(nvals, comps)
+plt.title("Plot of Percent Error of Summations vs Number of Terms in Summation")
+plt.xlabel("sum limit $N = 10^n$")
+plt.ylabel("Sum of $n^{-1}$")
+plt.savefig("3c.pdf")
--- problem_sets/ps1/locals/5/prob5a.py
+++ problem_sets/ps1/locals/5/prob5a.py
@@ -5,25 +5,41 @@ xmax = 40
 xmin = 0.25
 step = 0.1
 order = 10
-start = 50
+start = 100
 def down(x, n, m):
     j = np.zeros(start+2, np.float128)
     j[m+1] = j[m] = 1
     for k in range(m, 0, -1):
-        j[k-1] = ((2*k+1) /x)*j[k] - j[k+1]
-    scale = np.sin(x)/x/j[0]
+        j[k-1] = np.float(((2*k+1) /x)*j[k] - j[k+1])
+    scale = np.float128(np.sin(x)/x/j[0])
     return j[int(n)]*scale
 
 def up(x, n, m):
     j = np.zeros(start+2, np.float128)
     j[m+1] = j[m] = 1
     for k in range(m, start+1, 1):
-        j[k+1] = ((2.*k+1.) /x)*j[k] - j[k-1]
-    scale = np.sin(x)/x/j[0]
+        j[k+1] = np.float128(((2.*k+1.) /x)*j[k] - j[k-1])
+    scale = np.float128(np.sin(x)/x/j[0])
     return j[int(n)]*scale
 
-print("l \t \t x \t \t j_l(x) down \t \t j_l(x) up")
+def diff(u, d):
+    return np.abs(u - d) / (np.abs(u) + np.abs(d))
+print("l \t \t x \t \t j_l(x) down \t \t \t j_l(x) up \t \t \t % diff")
 for l in np.linspace(1, 25, 25):
     for x in [0.1, 1, 10]:
-        print(l, "\t \t", x, "\t \t", down(x, l, start), "\t"
-                ,up(x, l, 0))
+        d = down(x, l, start)
+        u = up(x, l, 0)
+        percdiff = diff(u, d)
+        print(l, "\t", x, "\t", d, "\t", u, "\t", percdiff)
+# Generate Graphs of first 100 bessel functions
+xdata = np.linspace(-95, 95, 1000)
+plt.figure()
+for l in np.linspace(0, 99, 100):
+    ydata = []
+    for x in xdata:
+        ydata.append(down(x, l, start))
+    plt.plot(xdata, ydata)
+plt.title("First 100th order bessel functions")
+plt.xlabel("x")
+plt.ylabel("$J_l(x)$")
+plt.savefig("bessel.pdf")
