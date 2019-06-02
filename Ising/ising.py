9cec3ab60bc0478eec2a667a0b2db65305cfe586
Yanall-Boutros <yanallboutros@gmail.com>
2019-06-01 19:51:26 -0700

    update

--- Ising/ising.py
+++ Ising/ising.py
@@ -1,51 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
 # As seen in lecture:
 # Ising model implementation with Metropolis algorithm
 # Based on IsingViz.py from Landau, et al.
 
-N     = 2000                 # number of spin sites                                   
-num_steps = 20*N           # number of iterations
-B     = 0.05                # magnetic field                               
-mu    = .33                # g mu (not needed if B=0)
-J     = 1.                 # exchange energy                              
+N     = (2000, 2000) # number of spin sites                                   
+num_steps = 20*N[0]*N[1]  # number of iterations
+B     = 0.05         # magnetic field                               
+mu    = .33          # g mu (not needed if B=0)
+J     = 1.           # exchange energy                              
+k     = 1.
+t     = 1.
 np.random.seed() 
-        
-def energy(S, J, mu, B):
-    FirstTerm = 0.
-    SecondTerm = 0.
-    for i in range(-1,N-1):
-        FirstTerm += -J * S[i]*S[i+1]
-    for i in range(0,N):   
-        SecondTerm += -mu*S[i]*B
-    return (FirstTerm + SecondTerm);
+state0 = -1*np.ones(N) # Start with an arbitrary spin configuration
+Evals = []
+def energy(State, J, mu, B):
+    # Energy will call row_energy on every row in State, and on 
+    # every row of its transpose.
+    total_energy = 0
+    for row in State:
+        total_energy += row_energy(row, J, mu, B)
+    for col in State.transpose():
+        total_energy += row_energy(col, J, mu, B)
+    return total_energy
+def row_energy(S, J, mu, B):
+    first_set = np.concatenate([np.array([S[-1]]), S[:-1]])
+    FirstTerm = np.sum(-J*first_set[:-1]*first_set[1:])
+    SecondTerm = np.sum(-mu*S*B)
+    return (FirstTerm + SecondTerm)
 
-def energy_change(S, i):
+def energy_change(S, coor):
     """Determine the change in energy if spin `i` is flipped
     
     `(i+1) % len(S)` implements the periodic boundary condition.
     """
-    return 2*J*(S[i-1]*S[i] + S[i]*S[(i+1) % len(S)]) + 2*B*mu*S[i]
+    x = coor[0]
+    y = coor[1]
+    # Multiply spin site by all adjacent elements
+    S_left  = S[x-1,y]
+    S_right = S[(x+1) % len(S),y]
+    S_up    = S[x,(y+1) % len(S)]
+    S_down  = S[x,y-1]
+    return 2*J*S[x,y]*(S_left + S_right + S_up + S_down % len(S)) + 2*B*mu*S[x,y]
 
-def ising(state0, num_steps, J, mu, B, kT):
+def TwoDIsing(state0, num_steps, J, mu, B, kT):
     ES = energy(state0, J, mu, B)
     energy_values = []
     energy_values.append(ES)
-    state = np.zeros([num_steps,N])        # spin states: up(1), down (-1)
-    state[0,:]=state0
-    for istep in range(1,num_steps):
-        test_state = list(state[istep-1,:])
-        random_site = int(N*np.random.random())
+    # Contains a copy of the state configuration so we don't have to store
+    # 2**N**2 * # of time step elements
+    state_configs = np.array([state0, state0])
+    deltas = [] # A lighter way of keeping track of how the state changes.
+    randx = np.random.randint(2000, size=num_steps)
+    randy = np.random.randint(2000, size=num_steps)
+    count = 1
+    for x, y in np.stack(randx, randy):
+        #test_state = state_configs[-1]
         # Trial step: flip spin at one random site
-        test_state[random_site] *= -1.
+        #test_state[x,y] *= -1.
         """Our key modification is here: instead of calculating the energy
         afresh, we only calculate the change therein.
         """
-        ET = ES + energy_change(state[istep-1], random_site)
+        state_configs[-1][x,y] *= -1
+        ET = ES + energy_change(state_configs[-1], (x,y))
         R = np.exp((ES-ET)/(kT))           # Boltzmann test
         if R > np.random.random():
-            state[istep,:] = test_state      # replace the state, or
+            #state_configs[-1] = test_state      # replace the state, or
             ES = ET
+            deltas.append((x,y))
         else:
             # advance the previous state forward
-            state[istep,:]=state[istep-1,:]
+            state_configs[-1]=state_configs[-2]
+            deltas.append(())
         energy_values.append(ES)
-    return state, energy_values
+        count += 1
+        if count % 1000 == 0: print((count / num_steps)*100," %.....................")
+    return state_configs, energy_values, deltas
